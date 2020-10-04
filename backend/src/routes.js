dde416f32c393635b192c808c2ff22cbe9ae13e9
rafaelfabiano <37879989+rafaelfabiano@users.noreply.github.com>
2020-10-04 12:26:55 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,11 +4,17 @@ const axios = require('axios');
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
+    
+    //informar o nome do usario que sera pesquisado
     const{ github_username} = request.body;
 
+    //puxar informações do github
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    console.log(apiResponse.data);
+    const {name = login, avatar_url, bio} = apiResponse.data;//repassando informações para uma variavel
+
+    console.log(name, avatar_url, bio, github_username);// exibir informações coletadas
+    
 
     return response.json({ message: 'Teste'});    
 });
